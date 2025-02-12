25b5abdc4b67484b72e5eae985f2503a2717cf9e
jbolanho <juliabolanho@gmail.com>
2025-02-12 11:49:30 -0300

    main

--- include/cub.h
+++ include/cub.h
@@ -6,23 +6,24 @@
 /*   By: jbolanho <jbolanho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 10:10:14 by jbolanho          #+#    #+#             */
-/*   Updated: 2025/02/11 12:39:34 by jbolanho         ###   ########.fr       */
+/*   Updated: 2025/02/12 11:33:27 by jbolanho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_H
 # define CUB_H
 
-// # include 
-// # include 
+# include "../libft/include/libft.h"
+# include "../libft/include/get_next_line.h"
+# include "../libft/include/ft_printf.h"
 // # include 
 // # include 
 
 
-// typedef struct s_cub
-// {
-    
-// }           t_cub;
+typedef struct s_game
+{
+	
+}		t_game;
 
 // 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jbolanho <jbolanho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 10:13:02 by jbolanho          #+#    #+#             */
-/*   Updated: 2025/02/11 12:41:35 by jbolanho         ###   ########.fr       */
+/*   Updated: 2025/02/12 11:33:13 by jbolanho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	
+	t_game	cub;
+
+	init(&cub);
+	validate(&cub, argv[1]);
+	game(&cub, argv[1]);
+	bye_bye(&cub);
+	return (0);
 }
