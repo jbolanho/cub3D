988881128825d284a0d6d1888b7a32f1e47f2bff
jbolanho <juliabolanho@gmail.com>
2025-02-13 17:35:31 -0300

    init game

--- include/cub.h
+++ include/cub.h
@@ -6,7 +6,7 @@
 /*   By: jbolanho <jbolanho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 10:10:14 by jbolanho          #+#    #+#             */
-/*   Updated: 2025/02/12 11:33:27 by jbolanho         ###   ########.fr       */
+/*   Updated: 2025/02/13 16:42:40 by jbolanho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,40 @@
 # include "../libft/include/libft.h"
 # include "../libft/include/get_next_line.h"
 # include "../libft/include/ft_printf.h"
-// # include 
+# include "MLX42.h"
+
 // # include 
 
 
 typedef struct s_game
 {
-	
+	mlx_t			*mlx;
+	t_map			map;
+	int				time;
+	size_t			window_wid;
+	size_t			window_hei;
+	mlx_texture_t	*no;
+	mlx_texture_t	*so;
+	mlx_texture_t	*ea;
+	mlx_texture_t	*we;
+	mlx_image_t		floor_ceiling;
 }		t_game;
 
 // 
 
-
+typedef struct s_map
+{
+	size_t		wid;
+	size_t		hei;
+	char		*cub_map;
+	char		*north_path;
+	char		*south_path;
+	char		*east_path;
+	char		*west_path;
+	uint32_t	*floor_color;
+	uint32_t	*ceiling_color;
+	
+}		t_map;
 
 
 
--- src/game.c
+++ src/game.c
@@ -6,11 +6,77 @@
 /*   By: jbolanho <jbolanho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 11:52:56 by jbolanho          #+#    #+#             */
-/*   Updated: 2025/02/12 11:54:25 by jbolanho         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:33:05 by jbolanho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/cub.h"
+
 void	game(t_game *cub)
+{
+	init_game(cub);
+	init_images(cub);
+	init_window(cub);
+	mlx_loop_hook(cub->mlx, key_press, cub);
+	mlx_loop()
+}
+
+void	init_game(t_game *cub)
+{
+	cub->window_wid = 800;
+	cub->window_hei = 600;
+	cub->mlx = mlx_init(cub->window_wid, cub->window_hei, "Severance", false);
+	if (!cub->mlx)
+	{
+		ft_printf("Error. MLX init error.\n");
+		bye_bye(cub, 1);
+	}
+	cub->time = mlx_get_time();
+	//cub->rotation_speed = 0.02;
+	// cub->move_speed = 0.06;
+}
+
+void	init_images(t_game *cub)
+{
+	//int	i;
+
+	cub->no = mlx_load_png(cub->map.north_path);
+	cub->so = mlx_load_png(cub->map.north_path);
+	cub->ea = mlx_load_png(cub->map.north_path);
+	cub->we = mlx_load_png(cub->map.north_path);
+
+}
+
+void	init_window(t_game *cub)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	cub->floor_ceiling = mlx_new_image(cub->mlx, cub->window_wid, cub->window_hei);
+	if (!cub->floor_ceiling)
+	{
+		ft_printf("Error. Floor Ceiling problem\n");
+		bye_bye(cub);
+	}
+	while (x < cub->window_hei)
+	{
+		while (y < cub->window_wid)
+		{
+			if (x < (cub->window_hei / 2))
+				mlx_put_pixel(cub->floor_ceiling, x, y, cub->map.ceiling_color);
+			else
+				mlx_put_pixel(cub->floor_ceiling, x, cub->map.floor_color);
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	mlx_image_to_window(cub->mlx)
+}
+
+void	key_press(t_game *cub)
 {
 	
 }
